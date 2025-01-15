0757e64f40707006e32a293342c652969e2c5859
Rahul <rahul.iraveni4800@gmail.com>
2025-01-15 11:33:51 +0530

    added sigup

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,16 +1,47 @@
+import { useState } from "react";
 import "../styles/loginPage.css";
 const Login = () => {
+
+    const [isLogin, setIsLogin] = useState(true);
+
     return (
         <div className="login-container">
             <h1>Login/Register</h1>
+           
             <form>
-                <input type="text" placeholder="Enter Your Name" />
-                <input type="email" placeholder="Enter Your Email" />
-                <input type="number" placeholder="Enter Your Phone Number" />
+            <div className="form-toggle">
+                <button className={isLogin ? "active" : ""}
+                onClick={(e)=>{
+                    e.preventDefault();
+                    setIsLogin(true)
+                }}
+                >Login</button>
+                <button className={!isLogin ? "active" : ""}
+                onClick={(e)=>{
+                    e.preventDefault();
+                    setIsLogin(false)
+                }}
+                >SignUp</button>
+            </div>
+            {isLogin ? <>
                 
+                <input type="email" placeholder="Enter Your Email" required />
+                <input type="password" placeholder="Enter Your Password" required/>
+
                 <div className="Login-btn">
                     <button>Login</button>
                 </div>
+                <p>Not a member? <a href="#" onClick={()=> setIsLogin(false)}>  Signup Now</a></p>
+            </> : <>
+            <input type="text" placeholder="Enter Your Name" required/>
+                <input type="email" placeholder="Enter Your Email" required/>
+                <input type="number" placeholder="Enter Your Phone Number" required />
+
+                <div className="Login-btn">
+                    <button>SignUp</button>
+                </div>
+            </>}
+                
 
 
             </form>
