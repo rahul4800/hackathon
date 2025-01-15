00570b4d904c906a2859d09d6fb417fcd6624037
Rahul <rahul.iraveni4800@gmail.com>
2025-01-15 12:19:23 +0530

    edited

--- src/admin/AdminLogin.jsx
+++ src/admin/AdminLogin.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "../styles/AdminLogin.css";
+import "../styles/adminLogin.css";
 
 
 const AdminLogin = ()=>{
